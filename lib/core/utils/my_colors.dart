2ba1d01563f854c548b6de218f24a441a67caa44
24shrouk <shroukhegab3@gmail.com>
2025-07-24 14:37:01 +0300

    App intialization add app colors add splach images

---
+++ lib/core/utils/my_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color navyColor = Color(0xff51526C);
+  static const Color greyScaleColor = Color(0xff7993C5);
+  static const Color greyColor = Color(0xff8C99B2);
+  static const Color lightBlue100Color = Color(0xff3F80FF);
+  static const Color lightBlue300Color = Color(0xff5286EC);
+  static const Color lightBlue400Color = Color(0xff658DD9);
+  static const Color lightBlue200Color = Color(0xff659AFF);
+  static const Color lightBlue500Color = Color(0xff8CB3FF);
+  static const Color lightBlue700Color = Color(0xffB2CCFF);
+  static const Color lightBlue900Color = Color(0xffD9E6FF);
+  static const Color darkBlue100Color = Color(0xff3F80FF);
+  static const Color darkBlue200Color = Color(0xff0056FE);
+  static const Color darkBlue500Color = Color(0xff0041BF);
+  static const Color darkBlue700Color = Color(0xff002B7F);
+  static const Color darkBlue900Color = Color(0xff001640);
+  static const Color darkRed100Color = Color(0xffFF3F50);
+  static const Color darkRed200Color = Color(0xffFE0017);
+  static const Color darkRed500Color = Color(0xffBF0011);
+  static const Color darkRed700Color = Color(0xff7F000B);
+  static const Color darkRed900Color = Color(0xff400006);
+}
